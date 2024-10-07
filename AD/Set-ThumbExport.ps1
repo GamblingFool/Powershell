0d0b26a9ba02d517534db603cc6332fbce703ecf
Caleb Grant <caleb.grant@ouh.nhs.uk>
2024-10-07 16:02:56 +0100

    Added completion statement

--- AD/Set-ThumbExport.ps1
+++ AD/Set-ThumbExport.ps1
@@ -1,3 +1,4 @@
 ﻿Remove-Variable * -Force -Scope global -ErrorAction SilentlyContinue
 Import-Module activedirectory
 Get-ADUser -Filter {samaccountname -eq "user.name"} -Properties * | select -ExpandProperty thumbnailphoto | Set-Content -Path "C:\Work\powershell\honThumb.jpg" -Encoding byte
+Write-Host 'All done'
